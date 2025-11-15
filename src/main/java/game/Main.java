44a99086a4abbfcfce9b9a9d0d468b6bf8a4ada4
Ghaithehasan <abrahymtrkyhsn0@gmail.com>
2025-11-14 16:59:48 -0800

    adding BFS Algorithm to the Infastructure for getting the solution

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,11 +1,15 @@
 package game;
+import game.search.SearchAlgorithms;
+import model.Action;
 import model.Level;
 import model.LevelLoader;
 import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        int the_number_of_level = 3;
+        int the_number_of_level = 10;
         System.out.println("╔════════════════════════════════════╗");
         System.out.println("║     Welcome to JUMP IN             ║");
         System.out.println("║     Level " + the_number_of_level + "                    ║");
@@ -20,6 +24,29 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e.getMessage());;
         }
+
+        System.out.println("=======================================================================");
+        System.out.println("=======================================================================");
+//
+//        try {
+//            Level level = LevelLoader.loadLevel(the_number_of_level);
+//            List<Action> solution = SearchAlgorithms.bfs(level);
+//
+//            if (solution != null) {
+//                System.out.println("تم العثور على حل!");
+//                int the_number_of_steps=0;
+//                for (Action action : solution) {
+//                    System.out.println(action);
+//                    the_number_of_steps++;
+//                }
+//
+//                System.out.println("the number of steps is : " + the_number_of_steps);
+//            } else {
+//                System.out.println("لم يتم العثور على حل لهذا المستوى.");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
 
     }
--- src/main/java/game/search/SearchAlgorithms.java
+++ src/main/java/game/search/SearchAlgorithms.java
@@ -1,4 +1,48 @@
 package game.search;
 
+import game.rules.GoalChecker;
+import game.rules.NodeGenerator;
+import model.Action;
+import model.Level;
+import model.Node;
+import model.State;
+
+import java.util.*;
+
 public class SearchAlgorithms {
+
+    public static List<Action> bfs(Level initialLevel)
+    {
+        Set<State> visited = new HashSet<>();
+        Queue<Node> queue = new LinkedList<>();
+        Node rootNode = new Node(null , initialLevel.createInitialState() , null , 0);
+
+        queue.add(rootNode);
+        visited.add(rootNode.getState());
+
+        while (!queue.isEmpty())
+        {
+            Node current_node = queue.poll();
+
+            if(GoalChecker.isFinal(current_node.getState()))
+            {
+                return current_node.getPath();
+            }
+
+            List<Node> childrenNodes = NodeGenerator.generateNextNodes(current_node);
+
+            for(Node child:childrenNodes)
+            {
+                if(!visited.contains(child.getState()))
+                {
+                    visited.add(child.getState());
+                    queue.add(child);
+
+                }
+            }
+        }
+
+        return null;
+
+    }
 }
