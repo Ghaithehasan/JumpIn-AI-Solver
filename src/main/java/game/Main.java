1799ea58519fd9424ac848ceb2720a5d28ba6a18
Ghaithehasan <abrahymtrkyhsn0@gmail.com>
2026-01-06 20:48:26 +0300

    adding Minimax Algorithm

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -10,54 +10,9 @@ public class Main {
 
     public static void main(String[] args) {
         int the_number_of_level = 10;
-//        System.out.println("╔════════════════════════════════════╗");
-//        System.out.println("║     Welcome to JUMP IN             ║");
-//        System.out.println("║     Level " + the_number_of_level + "                       ║");
-//        System.out.println("╚════════════════════════════════════╝");
-//        System.out.println();
-//
-//        try {
-//            Level level = LevelLoader.loadLevel(the_number_of_level);
-//            GamePlay game = new GamePlay(level);
-//            game.play();
-//
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());;
-//        }
-
-
-
-
 
         System.out.println("=======================================================================");
         System.out.println("=======================================================================");
-
-//        Level level = null;
-//        try {
-//            level = LevelLoader.loadLevel(10);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        State state = level.createInitialState();
-//        List<Action> actions = MoveGenerator.getPossibleActions(state);
-//
-//        for (Action a : actions) {
-//            if (a instanceof RabbitAction) {
-//                RabbitAction ra = (RabbitAction) a;
-//                Position from = findRabbitPosition(state, ra.getRabbit_id());
-//                Position to = ra.getNewPosition();
-//
-//                String direction = getDirection(from, to);
-//                System.out.println("Rabbit " + ra.getRabbit_id() + " → " + direction);
-//            } else if (a instanceof FoxAction) {
-//                FoxAction fa = (FoxAction) a;
-//                // نفس الشي للثعلب
-//            }
-//        }
-//
-//// Helper:
-
-
 
         try {
             Level level = LevelLoader.loadLevel(the_number_of_level);
@@ -80,7 +35,6 @@ public class Main {
             e.printStackTrace();
         }
     }
-
 
 
 }
